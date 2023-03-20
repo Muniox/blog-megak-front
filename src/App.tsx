20e00d6c92c2fcc23b5ae5bcc1e141cebd58c6a0
Muniox <muniox20@gmail.com>
2023-03-20 23:35:15 +0100

    Added Sidebar for test Tailwind.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React, { FC } from 'react';
+import Logo from './assets/logo.svg';
+import { Sidebar } from './Component/Sidebar';
 
 // @TODO zaimportować google fonts do reacta
 
 export const App: FC = () => (
-  <div className="container mx-auto h-screen border-4">
-    <header className="">
-      to jest header
+  <div className="container mx-auto border-4 fix-min-h-screen max-w-screen-xl relative">
+    <Sidebar />
+    <header className="px-2.5 mt-5">
+      <img src={Logo} alt="Logo JS DOSE - Your Daily Dose of JS" className="w-1/2 max-w-min" />
     </header>
     <main>
       to jest main
--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
       plugins: [
         tailwindcss(),
         autoprefixer({}),
-        postcssNested({})
+        postcssNested({}),
       ]
     }
   },
